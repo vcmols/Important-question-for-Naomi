24bcc03539e4265bde796420cbfbe16f0859fff0
vcmols <molsverac@gmail.com>
2026-01-30 23:40:10 +0100

    Update config.js

--- config.js
+++ config.js
@@ -77,7 +77,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: false,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "./valentine.mp3", // Music streaming URL
+        musicUrl: "./https://vcmols.github.io/Important-question-for-Naomi/valentine.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
