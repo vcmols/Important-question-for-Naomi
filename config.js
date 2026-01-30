c872569446b87cacff8a2ec77f645b5b63174707
vcmols <molsverac@gmail.com>
2026-01-30 23:37:52 +0100

    Update config.js

--- config.js
+++ config.js
@@ -77,7 +77,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: false,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "./Chappell_Roan_Red_Wine_Supernova_Lyrics_gxb6uk.mp3", // Music streaming URL
+        musicUrl: "./valentine.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
